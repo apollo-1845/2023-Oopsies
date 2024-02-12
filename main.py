21ad7f11f7c8a4198c564d350fd79b26a0a99989
spamtactics <108055980+spamtactics@users.noreply.github.com>
2024-02-12 10:01:03 +0000

    Making the code suitable for submission

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from picamera import PiCamera
 import os
 # we will be using time instead of the tutorial's suggested EXIF data because the time function returns the time elapsed
 # with significantly greater precision than the time recorded in the EXIF data.
-def checkTimeDiff(start):
-    return time.process_time()-start
 def convertToCV(image1,image2):
     cv_image1 = cv2.imread(image1,0)
     cv_image2 = cv2.imread(image2,0)
@@ -17,10 +15,8 @@ def calculateFeatures(image1,image2, feature_number):
     keypoints2, descriptors2=orb.detectAndCompute(image2,None)
     return keypoints1, keypoints2, descriptors1,descriptors2
 def calculateMatches(descriptors_1, descriptors_2):
-    # making this more efficient is going to be a priority. Possibly by implementing a binary search
     brute_force = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
     matches = brute_force.match(descriptors_1, descriptors_2)
-    # do we even need to sort the matches?
     matches = sorted(matches, key=lambda x: x.distance)
     return matches
 def convertToCoordinates(keypoints_1, keypoints_2, matches):
@@ -47,24 +43,31 @@ def getMeanDistance(coordinates_1, coordinates_2):
         totalDistance += distance
     return totalDistance / numberOfCoordinates
 def getSpeed(feature_distance, time_difference):
-    # I merged the calculation of *GSD/100000 into a single constant
+    # I merged the calculation of *GSD/100000 into the constant 0.12648
     distance = feature_distance * 0.12648
     speed = distance / time_difference
     return speed
+#this is the time with which the loop last started
 start_time=time.process_time
+#this stores the total time elasped
 total_time=0
+#this stores the time when the first picture was taken
 begin_time=0
+#this stores the interval between the two pictures
 end_time=0
-time_between_photos=0
 camera=PiCamera()
+#this stores the sum of all speeds calculated
 total_average_speeds=0
+#this stores the number of times the speed was calculated
 total_pictures=0
-while total_time<600:
-    #this code runs for 10 minutes, which is 600 seconds
+
+while total_time<590:
+    #this code runs just under 10 minutes, which is 600 seconds
+    #this ensures that there is enough time to write the speed onto a file
     begin_time=time.process_time()
     camera.capture("Photo1.jpg")
     image_1="Photo1.jpg"
-    #apparently, it needs time to sleep
+    #using a 5 second interval to give the camera time to sleep
     sleep(5)
     camera.capture("Photo2.jpg")
     #process_time doesn't actually account for sleep time, so I have to add it
@@ -74,14 +77,18 @@ while total_time<600:
     image_1_cv, image_2_cv = convertToCV(image_1, image_2)
     keypoints_1, keypoints_2, descriptors_1, descriptors_2 = calculateFeatures(image_1_cv, image_2_cv, 1000)
     matches = calculateMatches(descriptors_1, descriptors_2)
-    # for whatever reason, the program isn't finding any matches
     coordinates_1, coordinates_2 = convertToCoordinates(keypoints_1, keypoints_2, matches)
     pixel_Distance=getMeanDistance(coordinates_1,coordinates_2)
     total_average_speed+=getSpeed(pixel_Distance, end_time)
     total_pictures+=1
+    #incrementing the time that elapssed during that loop. 
     total_time+=time.process_time-start_time
+    #resetting the start time
     start_time=time.process_time
+    
+#using the formula mean = Σx/n
 mean_speed=total_average_speed/total_pictures
+
 #rounding mean speed to 5 s.f.
 string_speed=str(mean_speed)
 rounded_speed=""
@@ -92,6 +99,7 @@ while significant_figures<6:
         rounded_speed+=string_speed[index]
         significant_figures+=1
     index+=1
+    
 #adding it to a file
 with open("result.txt","w") as file:
     file.write(rounded_speed)
